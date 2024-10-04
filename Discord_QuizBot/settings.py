1ed6748a8adf6e9d64c8e7813c40c7b596dc27cb
shanky2003g <shashankgupta2473@gmail.com>
2024-10-04 18:35:30 +0530

    Setting environment varibles for API_KEY

--- Discord_QuizBot/settings.py
+++ Discord_QuizBot/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+# Load environment variables from .env file 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6pey^3p(7rx6ucfe%!_$u*&e%fy&4r8fuo_-@czwt^fxu%3ky$"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG')
+API_KEY =os.getenv('API_KEY')
 
 ALLOWED_HOSTS = []
 
