2b143da34e92f99d2eeb01349fbdeb6e689cfd08
shanky2003g <shashankgupta2473@gmail.com>
2024-10-12 01:13:20 +0530

    revert commit

--- Discord_QuizBot/bot.py
+++ Discord_QuizBot/bot.py
@@ -8,7 +8,7 @@ import time
 from settings import BASE_DIR
 load_dotenv(BASE_DIR/".env") 
 import os
-token = str(os.getenv('TOKEN'))
+token = os.getenv('TOKEN')
 c_id = os.getenv('ID')
 # Enable necessary intents
 intents = discord.Intents.default()
